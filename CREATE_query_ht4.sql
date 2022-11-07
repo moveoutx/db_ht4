3e092a2c7d716b01bf554068b9aa59a9e20317b6
Михаил Иванов <moveout@yandex.ru>
2022-11-07 17:17:07 +0300

    time add

--- CREATE_query_ht4.sql
+++ CREATE_query_ht4.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS Artist (
 	id SERIAL PRIMARY KEY,
-	Artist_name VARCHAR(80) NOT NULL
+	Artist_name VARCHAR(80) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Genre (
 	id SERIAL PRIMARY KEY,
-	Genre_name VARCHAR(80) NOT NULL
+	Genre_name VARCHAR(80) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Genre_Artist (
@@ -16,14 +16,14 @@ CREATE TABLE IF NOT EXISTS Genre_Artist (
 
 CREATE TABLE IF NOT EXISTS Collection (
 	id SERIAL PRIMARY KEY,
-	Collection_year DATE,
+	Collection_year date default null,
 	Collection_name VARCHAR(60)
 );
 
 CREATE TABLE IF NOT EXISTS Album (
 	id SERIAL PRIMARY KEY,
-	Album_name VARCHAR(60),
-	Album_year DATE
+	Album_name VARCHAR(60) NOT NULL,
+	Album_year DATE check(Album_year > '1900-01-01')
 );
 
 CREATE TABLE IF NOT EXISTS Artist_Album (
@@ -34,9 +34,13 @@ CREATE TABLE IF NOT EXISTS Artist_Album (
 
 CREATE TABLE IF NOT EXISTS Track (
 	id SERIAL PRIMARY KEY,
-	Track_name VARCHAR(60),
-	Duration TIME,
-	AlbumID INTEGER references Album(id),
-	CollectionID INTEGER references Collection(id)
+	Track_name VARCHAR(60) UNIQUE NOT NULL,
+	Duration TIME NOT NULL,
+	AlbumID INTEGER references Album(id)
 );
 
+CREATE TABLE IF NOT EXISTS Collection_Track (
+	Collection_ID INTEGER REFERENCES Collection(id),
+	Track_ID INTEGER REFERENCES Track(id),
+	CONSTRAINT pk2 PRIMARY KEY (Collection_ID, Track_ID)
+);
